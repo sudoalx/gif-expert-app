70b3a99a8eba9b792c9c85cc1ad32c26665678e0
Alex <alex@alex-dev.me>
2023-11-21 03:07:59 -0600

    Refactor AddCategory component to include form and add category functionality

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,19 +1,35 @@
 import { useState } from "react";
 
-const AddCategory = () => {
-  const [inputValue, setInputValue] = useState("One Punch");
-  const onInputChange = (event) => {
-    setInputValue(event.target.value); // extract the value from the event object and set it to the state
+const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+  const onInputChange = ({ target }) => {
+    // destructuring the event object to get the target property
+    setInputValue(target.value); // target.value is the value of the input
+  };
+  const onSubmit = (e) => {
+    e.preventDefault(); // prevents the page from refreshing
+    handleAddCategory();
+  };
+  const handleAddCategory = () => {
+    if (inputValue.trim().length <= 1) return;
+    setCategories((categories) => [inputValue, ...categories]);
+    setInputValue("");
   };
   return (
-    <div className="centered-input">
-      <input
-        type="text"
-        placeholder="Search GIFs"
-        value={inputValue}
-        onChange={onInputChange} // the event is passed by default
-      />
-    </div>
+    <>
+      <form
+        className="centered-input"
+        onSubmit={onSubmit} /** the event is passed by default */
+      >
+        <input
+          type="text"
+          placeholder="Search GIFs"
+          value={inputValue}
+          onChange={onInputChange} // the event is passed by default
+        />
+      </form>
+      <button onClick={handleAddCategory}>Add Category</button>
+    </>
   );
 };
 
