c7015d0464c2174885ef230ed23cdaf7079a83e7
Alex <alex@alex-dev.me>
2023-11-20 20:50:00 -0600

    Initial branding

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+const GifExpertApp = () => {
+  return (
+    <>
+      {/* title */}
+      <h1>GIF Expert App</h1>
+      {/* search input */}
+      {/* GIFS container */}
+      {/* GIF Items */}
+    </>
+  );
+};
+
+export default GifExpertApp;
