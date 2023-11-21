06dae72887fea6bd57fd1e14f64cf37975e6d56a
Alex <alex@alex-dev.me>
2023-11-21 03:55:44 -0600

    Add GifGrid component to render GIF items

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
 const GifExpertApp = () => {
   const [categories, setCategories] = useState([]);
   const handleAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     setCategories([newCategory, ...categories]);
   };
   return (
     <>
       <h1>GIF Expert App</h1>
       <AddCategory onNewVal={(value) => handleAddCategory(value)} />
-      {/* GIFS container */}
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
-      {/* GIF Items */}
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
