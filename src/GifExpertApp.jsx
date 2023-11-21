379d0f032c3b2d51a0efa64889d36de8f8b6c4bf
Alex <alex@alex-dev.me>
2023-11-20 21:21:46 -0600

    Made onAddCategory action button Used ... to push items to the useState array

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Marvel"]);
+  const onAddCategory = () => {
+    // TODO: Add category when the button is pressed
+    setCategories((oldCategories) => [...oldCategories, "Android"]);
+  };
   return (
     <>
       {/* title */}
       <h1>GIF Expert App</h1>
       {/* search input */}
       {/* GIFS container */}
+      <button
+        onClick={() => {
+          onAddCategory();
+        }}
+      >
+        Add category
+      </button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
