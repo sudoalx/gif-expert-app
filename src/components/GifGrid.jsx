8634acf2cea3cf08ac53a2cdf6165fc1f0422e0a
Alex <alex@alex-dev.me>
2023-11-21 13:51:45 -0600

    Add react-dotenv dependency and update GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,10 +1,22 @@
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+import getGifs from "../helpers/getGifs";
+
 const GifGrid = ({ category }) => {
+  useEffect(() => {
+    getGifs(category);
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
       <p>Hello world</p>
     </>
   );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 };
 
 export default GifGrid;
