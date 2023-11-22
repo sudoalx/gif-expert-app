9091bb10d79291326987b0492c644c9192cb306b
Alex <alex@alex-dev.me>
2023-11-21 22:04:02 -0600

    Update limit of gifs to 12

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=12`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(({ id, title, images }) => {
