6a61a4403e973405f5183f155184b2445ea1d8bd
Alex <alex@alex-dev.me>
2023-11-21 22:59:38 -0600

    Make loading elements

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,6 +8,12 @@ const GifGrid = ({ category }) => {
   return (
     <div className="grid-container">
       <h3>{category}</h3>
+      {isLoading && (
+        <div className="loading-container">
+          <h2 className="loading-text">Loading...</h2>
+        </div>
+      )}
+
       <div className="card-grid">
         {images.map((image) => {
           return <GifItem key={image.id} {...image} />;
