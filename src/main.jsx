1ef7e47c5cb0f51dd69c75bc7a818b0f945164b5
Alex <alex@alex-dev.me>
2023-11-21 22:03:33 -0600

    Footer component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GifExpertApp from "./GifExpertApp";
 import "./styles.css";
+import Footer from "./components/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GifExpertApp />
+    <Footer />
   </React.StrictMode>
 );
