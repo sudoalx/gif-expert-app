9942eb33b6aa0395c4af58f73b2f87a6701f42b6
Alex <alex@alex-dev.me>
2023-11-21 23:08:29 -0600

    Fix import order in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GifExpertApp from "./GifExpertApp";
-import "./styles.css";
 import Footer from "./components/Footer";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
