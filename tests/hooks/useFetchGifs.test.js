c9e88486c40216a084c4a59cf9af67b5e4752f8d
Alex <alex@alex-dev.me>
2023-11-23 12:00:16 -0600

    Add tests for useFetchGifs custom hook

--- tests/hooks/useFetchGifs.test.js
+++ tests/hooks/useFetchGifs.test.js
@@ -1,5 +1,21 @@
-describe('Test useFetchGifs custom hook', () => { 
-    test('should return initial state', () => { 
-        
-     })
- })
+import { renderHook, waitFor } from "@testing-library/react"
+import { useFetchGifs } from "../../src/hooks/useFetchGifs"
+
+describe('Test useFetchGifs custom hook', () => {
+    test('should return initial state', () => {
+        const { result } = renderHook(() => useFetchGifs('Iron Man'))
+        const { images, isLoading } = result.current
+        expect(images.length).toBe(0)
+        expect(isLoading).toBe(true)
+    })
+
+    test('should return an array of images and isLoading should be false', async () => {
+        const { result } = renderHook(() => useFetchGifs('Iron Man'))
+        await waitFor(
+            () => expect(result.current.images.length).toBeGreaterThan(0)
+        )
+        const { images, isLoading } = result.current
+        expect(images.length).toBeGreaterThan(0)
+        expect(isLoading).toBe(false)
+    })
+})
